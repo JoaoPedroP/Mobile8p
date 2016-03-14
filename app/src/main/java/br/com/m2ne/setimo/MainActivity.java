00e17ef36fc59a3cb698e63db83987a1d51be2e1
Nonilton <nonilton@hotmail.com>
2016-03-14 20:30:06 -0300

    Trabalhando com listview e metodos onItemClick e onItemClickLongo

--- app/src/main/java/br/com/m2ne/setimo/MainActivity.java
+++ app/src/main/java/br/com/m2ne/setimo/MainActivity.java
@@ -3,15 +3,27 @@ package br.com.m2ne.setimo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     private EditText mensagem;
     private TextView visualizar;
     private Button enviar;
+    private List<String> listaMensagen;
+    private ArrayAdapter<String> adp;
+    private ListView lista;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,19 +32,47 @@ public class MainActivity extends AppCompatActivity {
 
         mensagem = (EditText) findViewById(R.id.edmensagem);
         enviar = (Button) findViewById(R.id.btEnviar);
-        visualizar = (TextView) findViewById(R.id.txtretorno);
+        lista = (ListView) findViewById(R.id.lista);
 
-     /*   enviar.setOnClickListener(new View.OnClickListener() {
+        this.listaMensagen = new ArrayList<>();
+
+
+        this.adp = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, android.R.id.text1, this.listaMensagen);
+        this.lista.setAdapter(this.adp);
+
+       /* this.lista.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onClick(View v) {
-                visualizar.setText(mensagem.getText().toString());
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                removeMensagem(position);
+            }
+        }); */
+
+
+        this.lista.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                removeMensagem(position);
+                return true;
             }
         });
-       */
     }
 
     public void mostrarMensagem(View v){
-        Toast.makeText(this, mensagem.getText().toString(), Toast.LENGTH_SHORT).show();
-        //visualizar.setText(mensagem.getText().toString());
+
+        if (this.mensagem.getText().toString().equals("")){
+            Toast.makeText(this, "Por favor informe o texto da mensagem", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        this.listaMensagen.add(mensagem.getText().toString());
+        this.adp.notifyDataSetChanged();
     }
+
+    private void removeMensagem(int position){
+        this.listaMensagen.remove(position);
+        this.adp.notifyDataSetChanged();
+
+    }
+
+
 }
